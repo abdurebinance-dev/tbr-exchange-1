4c0f8c008566c9fdc303a7b1267cdad94687c3c4
abdurebinance-dev <your_email@gmail.com>
2026-09-10 17:04:46 +0300

    Fix server config and request size limit

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const publicPath = path.join(process.cwd(), 'public');
 app.use(express.static(publicPath));
